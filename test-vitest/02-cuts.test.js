b37f9e4f6c7493f9115f6bb4ebdd602914a7d620
Peter Naydenov <peter.naydenov@gmail.com>
2025-10-23 11:21:23 +0300

    Test: Work in progress...

--- test-vitest/02-cuts.test.js
+++ test-vitest/02-cuts.test.js
@@ -60,5 +60,97 @@ describe ( 'Cuts integration', () => {
 
 
 
+    it ( 'List shortcuts', async () => {
+                    const script = cuts ()
+                    const calls = [];
+
+                     const scenes = [
+                                    { 
+                                            name: 'top'
+                                        , scene: { 
+                                                    show: ({task}) => { calls.push('show top'); task.done() }, 
+                                                    hide: ({task}) => { calls.push('hide top'); task.done() } ,
+                                                    'click: left-1': () => calls.push('click left-1') 
+                                            }
+                                        }
+                                ];
+
+                    script.setScenes ( scenes )
+                    expect ( script.listScenes() ).toEqual ( ['top'] )                    
+        }) // it List shortcuts
+
+
+
+    it ( 'beforeHide', async () => {
+                    const script = cuts ();
+                    const calls = [];
+                    const scenes = [
+                                    { 
+                                          name: 'top'
+                                        , scene: { 
+                                                    show: ({task}) => { calls.push('show top'); task.done() }, 
+                                                    hide: ({task}) => { calls.push('hide top'); task.done() } ,
+                                                    // Prevent unload event:
+                                                    'beforeHide': ({ done }) => done ( false ) ,
+                                                    'click: left-1': () => calls.push('click left-1') 
+                                            }
+                                        },
+                                        {
+                                          name: 'other'
+                                        , scene: {
+                                                    show: ({task}) => { calls.push('show other'); task.done() }, 
+                                                    hide: ({task}) => { calls.push('hide other'); task.done() }
+                                            }
+                                        }
+                                ];
+
+                    script.setScenes ( scenes )
+                    await script.show ({ scene: 'top' })
+                    expect ( calls ).toEqual (['show top'])
+
+                    await script.show ({ scene: 'other' })
+                    expect ( calls ).toEqual (['show top'])
+                    expect ( script.getState().scene ).toEqual ( 'top' )
+        }) // it beforeHide
+
+
+
+    it ( 'Call no existing scene', async () => {
+                    const script = cuts ({ logLevel : 1 });
+                    const calls = [];
+                    const scenes = [
+                                    { 
+                                          name: 'top'
+                                        , scene: { 
+                                                    show: ({task}) => { calls.push('show top'); task.done() }, 
+                                                    hide: ({task}) => { calls.push('hide top'); task.done() } ,
+                                                    'click: left-1': () => calls.push ( 'click left-1' ),
+                                                    '@app-error': ( arg ) => {
+                                                                calls.push ( 'app-error' )
+                                                                expect ( arg.type ).toBe ( 'error' )
+                                                                expect ( arg.message ).toBe ( 'Scene "any" is not available.' )
+                                                            }
+                                            }
+                                        },
+                                        {
+                                          name: 'other'
+                                        , scene: {
+                                                    show: ({task}) => { calls.push('show other'); task.done() }, 
+                                                    hide: ({task}) => { calls.push('hide other'); task.done() }
+                                            }
+                                        }
+                                ];
+
+                    script.setScenes ( scenes )
+                    await script.show ({ scene: 'top' })
+                    expect ( calls ).toEqual (['show top'])
+
+                     await script.show ({ scene: 'any' })
+                     expect ( calls ).toEqual (['show top', 'app-error'])
+                     expect ( script.getState().scene ).toEqual ( 'top' )
+        }) // it Call no existing scene
+
+
+
 
 }) // describe
